3a50c74794e0726acd061b69c7238fac1a838548
Koichiro Takita <takita.koichiro.4pz@jp.nssol.nipponsteel.com>
2025-08-29 11:23:44 +0900

    mypage.htmlを分離

--- insertHTML.js
+++ insertHTML.js
@@ -4,12 +4,13 @@ const sections = {
         ranking: 'ranking.html',
         //   recommendation: 'recommendation.html',
         //   popular: 'popular.html'
+        mypage: 'mypage.html',
         };
 
         for (const [id, file] of Object.entries(sections)) {
         fetch(file)
-            .then(res => res.text())
-            .then(html => {
-            document.getElementById(id).innerHTML = html;
-            });
-    }
+                .then(res => res.text())
+                .then(html => {
+                document.getElementById(id).innerHTML = html;
+                });
+        }
